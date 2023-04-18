70df0c5f9ba8b02b2083dd22973f2f827e8cc146
ma1ik-1 <126670879+ma1ik-1@users.noreply.github.com>
2023-04-18 15:59:01 +0100

    Add files via upload

---
+++ week-one/src/main/java/com/qa/springdemo/WeekOneApplication.java
@@ -0,0 +1,13 @@
+package com.qa.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeekOneApplication.class, args);
+	}
+
+}
---
+++ week-one/src/test/java/com/qa/springdemo/WeekOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeekOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
